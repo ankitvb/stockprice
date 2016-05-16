4808a18a8067f373b0d6bc0a7c092ed3e8dee323
Ankit Bhagatwala <ankitb@136.215.128.198.in-addr.dhcp.lbnl.us>
2016-05-16 10:32:31 -0700

    Changing get to post

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ def main():
   return redirect('/index')
 
 @app.route('/index')
-def index(request):
-  if request.POST:
-    ticker = request.POST['ticker']
-    close = request.POST['close']
-    adjClose = request.POST['adjClose']  
-    open_ = request.POST['open']
-    adjOpen = request.POST['adjOpen']
-
-    print ticker, close, open_
+def index():
+#  if request.POST:
+#    ticker = request.POST['ticker']
+#    close = request.POST['close']
+#    adjClose = request.POST['adjClose']  
+#    open_ = request.POST['open']
+#    adjOpen = request.POST['adjOpen']
+#
+#    print ticker, close, open_
 
   return render_template('index.html')
 
