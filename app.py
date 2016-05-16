e272e2921b00ab2d761a06959f47660eae7d6fc2
Ankit Bhagatwala <ankitb@136.215.128.198.in-addr.dhcp.lbnl.us>
2016-05-16 14:48:14 -0700

    Error checking for API call

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def get_data(stock_args):
     print headers
   
     # Dump data into Pandas dataframe
-    df = pd.DataFrame(data, columns=headers)
+    df = pd.DataFrame(data[1:], columns=headers)
     print df 
 
 
