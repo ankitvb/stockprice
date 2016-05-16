f45c7c206348da5bd719fc6671c514f96e45c72f
Ankit Bhagatwala <ankitb@Ankits-MacBook-Pro.local>
2016-05-16 16:52:13 -0700

    Adding Bokeh plots

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+from bokeh.plotting import figure, output_file, show
 import requests
 import pandas as pd
 
@@ -10,6 +11,7 @@ def main():
 
 @app.route('/index', methods=['GET','POST'])
 def index():
+  """Generating the main user facing page"""
   # See if we have params to process
   try:
     if request.method == 'GET':
@@ -26,6 +28,8 @@ def index():
   return render_template('index.html')
 
 def get_data(stock_args):
+  """Getting data from Quandl API and putting it into Pandas dataframe
+  """
   qdl_base_url = "https://www.quandl.com/api/v3/datasets/WIKI/"
   api_key = "QndbBLZGfwiqgg_MkvWW" 
   
@@ -44,7 +48,8 @@ def get_data(stock_args):
                'Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']
 
   # Parameters for call to the Quandl API
-
+  start_date = "" 
+  end_date = ""
   # Get column ids for data of interest
   col_list = [data_cols.index(translate[key]) for key in stock_args if key not in ['ticker']]
   print col_list
@@ -56,7 +61,7 @@ def get_data(stock_args):
 
   res = requests.get(qdl_full_url)
   if res.status_code != 200:
-    print "Could not get data from Quandl"
+    print "Could not get data from Quandl. Check ticker name"
   else:
     data = [list(line.split(',')) for line in res.iter_lines()] 
     headers = data[0]
@@ -67,10 +72,30 @@ def get_data(stock_args):
     df = pd.DataFrame(data[1:], columns=headers)
     print df 
 
+    plot(df,stock_args['ticker'])
 
-#@app.route('/plot')
-#def plot(request_args):
+  return   
 
+@app.route('/plot')
+def plot(stock_df,symbol):
+  """Generate a embedded html plot from dataframe data with Bokeh
+  """
+  # output to static HTML file
+  output_file("plot.html")    
+  
+  # Create plot
+  p = figure(
+      tools="pan,box,zoom,reset,save",    
+      y_axis_label="Price", y_range=[0.0,10000.0], title=symbol,
+      x_axis_label="Day"
+      )
+
+  p.line(stock_df['Date'], stock_df['Open'], legend="Open", line_color="blue")
+  p.line(stock_df['Date'], stock_df['Close'], legend="Close", line_color="red")
+
+  show(p)
+
+  return
 
 #  return render_template('plot.html')
 
