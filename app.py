d244d7e2b54252a290ca012105bf0091c7d08b92
Ankit Bhagatwala <ankitb@136.215.128.198.in-addr.dhcp.lbnl.us>
2016-05-16 13:37:40 -0700

    Getting stock data from Quandl

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import requests
 
 app = Flask(__name__)
 
@@ -11,7 +12,8 @@ def index():
   try:
     if request.method == 'GET':
       if request.args is not None:
-        print dict(request.args.to_dict().items())
+        stock_args = dict(request.args.to_dict().items())
+        get_data(stock_args)
       else:
         print "Args not found"
   except:
@@ -21,8 +23,31 @@ def index():
 
   return render_template('index.html')
 
-#def get_data(request_args):
+def get_data(stock_args):
+  qdl_base_url = "https://www.quandl.com/api/v3/datasets/WIKI/"
+  api_key = "QndbBLZGfwiqgg_MkvWW" 
   
+  translate = {'open':'Open',
+               'close':'Close',
+               'volume':'Volume',
+               'adjOpen':'Adj. Open',
+               'adjClose':'Adj. Close',
+               'adjVolume':'Adj. Volume'
+              }
+
+  cols = ['Date','Open','High','Low','Close','Volume',
+          'Ex-Dividend','Split' 'Ratio',
+          'Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']
+
+  
+  qdl_full_url = qdl_base_url + stock_args['ticker'] + ".csv"
+               + "?api_key="+api_key
+  print qdl_full_url
+
+  res = requests.get(qdl_full_url)
+  data = [line for line in res.iter_lines()]  
+
+  print data
 
 #@app.route('/plot')
 #def plot(request_args):
