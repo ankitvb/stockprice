3cc5f9bb873537de959e1265297429c5c90d094a
Ankit Bhagatwala <ankitb@136.215.128.198.in-addr.dhcp.lbnl.us>
2016-05-17 15:38:51 -0700

    Using charts.Line instead of plotting

--- app.py
+++ app.py
@@ -89,24 +89,12 @@ def get_data(stock_args):
 def plot(stock_df,symbol):
   """Generate a embedded html plot from dataframe data with Bokeh
   """
-  #output_file("plot.html")    
-
-  #dates = stock_df['Date'].tolist()
-  #opens = stock_df['Open'].tolist()
-  #closes = stock_df['Close'].tolist()
   reduced_df = stock_df[['Date','Open','Close']].copy()
-  reduced_df = reduced_df.head(n=5)
+  reduced_df = reduced_df.head(n=10)
 
-  print reduced_df
   # Create plot
-  #p = figure(title='GOOG', y_axis_label='Price', x_axis_label='Date', x_axis_type='datetime')
   p = Line(reduced_df, x='Date', y='Open', legend="top_right", ylabel="Price")
-
-  #p.line(dates, closes, legend="Close", line_color="red")
-
-  #xyvalues = np.array([[2, 3, 7, 5, 26], [12, 33, 47, 15, 126], [22, 43, 10, 25, 26]])
-
-  #p = Line(xyvalues, title="line", legend="top_left", ylabel='Languages')
+  p = Line(reduced_df, x='Date', y='Close', legend="top_right", ylabel="Price")
 
   script, div = components(p,CDN)
   
